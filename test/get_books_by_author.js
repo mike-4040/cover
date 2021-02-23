1f71fdefb9e72501aa024a38acad8d01cd65ab9c
Mike Kravtsov <mike@clickup.com>
2021-02-22 19:11:49 -0800

    updated scripts

--- test/get_books_by_author.js
+++ test/get_books_by_author.js
@@ -2,10 +2,19 @@ const { expect } = require('chai');
 const sdk = require('./sdk');
 
 describe('get books by author id', () => {
-  it('calls api', async () => {
+  it('get only books by author', async () => {
     const { data } = await sdk.get_books_by_author(2);
 
     expect(data).to.be.a('array');
     expect(data.length).equal(2);
+  })
+});
+
+describe('get all books', () => {
+  it('get a list of all books', async () => {
+    const { data } = await sdk.get_all_books();
+
+    expect(data).to.be.a('array');
+    expect(data.length).equal(4);
   })
 });
--- test/sdk.js
+++ test/sdk.js
@@ -1,4 +1,11 @@
-const axios = require('axios');
+const axios = require("axios");
 
-exports.get_books_by_author = async (author_id) =>
-  await axios.get(`http://localhost:3000/books/${author_id}`);
+const port = process.env.PORT || 3000;
+
+const base_url = `http://localhost:${port}`
+
+exports.get_books_by_author = async author_id =>
+  await axios.get(`${base_url}/books/${author_id}`);
+
+exports.get_all_books = async () =>
+  await axios.get(`${base_url}/books`);
