e90a7757f51b182065924083fd6af1bedf6767d1
Mike Kravtsov <mike@clickup.com>
2021-02-22 22:13:01 -0800

    added a line that never run

--- controllers/books.js
+++ controllers/books.js
@@ -4,6 +4,9 @@ exports.getAllBooks = (_req, res) => res.status(200).send(books);
 
 exports.getBooksByAuthor = (req, res) => {
   const req_author_id = Number(req.params.author_id);
+  if( req_author_id > 100) {
+    console.log('Never run')
+  }
   const book_of_author = books.filter(
     ({ author_id }) => author_id === req_author_id
   );
