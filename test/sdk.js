a7518027fc5ba9cf35282aad85b31a4a95cd0a4e
Mike Kravtsov <mike@clickup.com>
2021-02-23 17:59:00 -0800

    some update

--- test/sdk.js
+++ test/sdk.js
@@ -2,10 +2,8 @@ const axios = require("axios");
 
 const port = process.env.PORT || 3000;
 
-const base_url = `http://localhost:${port}`
+const base_url = `http://localhost:${port}`;
 
-exports.get_books_by_author = async author_id =>
-  await axios.get(`${base_url}/books/${author_id}`);
+exports.get_books_by_author = async (author_id) => await axios.get(`${base_url}/books/${author_id}`);
 
-exports.get_all_books = async () =>
-  await axios.get(`${base_url}/books`);
+exports.get_all_books = async () => await axios.get(`${base_url}/books`);
